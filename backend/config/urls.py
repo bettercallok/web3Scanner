30af0f80a477ea33ee837abfdcffac1165b1fec7
bettercallok <khatriom1108@gmail.com>
2026-06-13 16:10:25 +0530

    fix: Remove media static() from urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("scanner.urls")),
     path("api/reports/", include("reports.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
