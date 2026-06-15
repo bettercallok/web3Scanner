86f9162185fecec634350c9471bec906ecd25f33
bettercallok <khatriom1108@gmail.com>
2026-06-15 18:28:03 +0530

    fix(security): add user rate limiting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -106,10 +106,12 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {
         "anon": "20/hour",
         "burst": "5/minute",
+        "user": "50/hour",
     },
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
