b07645196f5f99f3e54e526a2da222178441ea7a
bettercallok <khatriom1108@gmail.com>
2026-06-15 18:28:11 +0530

    fix(security): enforce strong password validation on registration

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,10 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email')
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
